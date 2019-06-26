b32ae7c9b01c0636681bf37ca73091445f899193
zongxiaobai <17682309829@163.com>
2019-06-26 17:08:44 +0800

    fix:补充遗漏问题

---
+++ bin/yogurt.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+// 引入依赖
+var program = require('commander');
+ 
+// 定义版本和参数选项
+program
+  .version('0.1.0', '-v, --version')
+ 
+program
+  .command('init')
+  .description('初始化脚手架模板')
+  .action((cmd) => {
+    require('../lib/init')(cmd);
+  })
+program.on('--help', function(){
+  console.log('');
+  console.log('    我爱喝酸奶！');
+  console.log('');
+});
+ 
+program.parse(process.argv);
+ 
